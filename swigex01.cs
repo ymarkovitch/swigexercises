9f7625375e2fd7e6a7a2f91913ad19cf5d6a9d4c
Yakov Markovitch <ymarkovitch@gmail.com>
2015-10-30 22:05:00 +0300

    More

--- swigex01.cs
+++ swigex01.cs
@@ -5,12 +5,26 @@ using System.Collections.Generic ;
 using System.Runtime ;
 using System.Runtime.InteropServices ;
 using System.Security ;
-using SWIG.Exercises ;
+//using SWIG.Exercises ;
+
+public struct Foo
+{
+    public int v ;
+    public static explicit operator bool(Foo f) { return f.v != 0 ; }
+    public static bool operator !(Foo f) { return !(bool)f ; }
+    public static bool operator true(Foo f) { return (bool)f ; }
+    public static bool operator false(Foo f) { return !f ; }
+}
 
 public class SwigEx01
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!") ;
+        var f = new Foo() ;
+        f.v = 5 ;
+        if (f)
+            Console.WriteLine("Hello, World!") ;
+        if (!f)
+            Console.WriteLine("Bye, Baby!") ;
     }
 }
